50c8d4076e147259b0ff03e209dafa01a0fa23fb
Lovinder Pnag <lspnag@gmail.com>
2016-02-29 23:16:06 -0500

    refactor tests x 2

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -7,6 +7,11 @@ public class BowlingGameTest {
 
     private Game g;
 
+    private void rollMany(int n, int pins) {
+        for(int i = 0; i < n; i++)
+            g.roll(pins);
+    }
+
     @Before
     public void setUp() {
         g = new Game();
@@ -14,15 +19,13 @@ public class BowlingGameTest {
 
     @Test
     public void testGutterGame() {
-        for(int i = 0; i < 20; i++)
-            g.roll(0);
+        rollMany(20, 0);
         assertEquals(0, g.score());
     }
 
     @Test
     public void testAllOnes() {
-        for(int i = 0; i < 20; i++)
-            g.roll(1);
+        rollMany(20, 1);
         assertEquals(20, g.score());
     }
 }
