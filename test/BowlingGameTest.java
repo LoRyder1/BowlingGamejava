c14b4a7146490d64d268783b78d9fa78f20833f9
Lovinder Pnag <lspnag@gmail.com>
2016-03-01 00:42:26 -0500

    test for perfect game

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -46,6 +46,12 @@ public class BowlingGameTest {
         assertEquals(24, g.score());
     }
 
+    @Test
+    public void testPerfectGame() {
+        rollMany(20,10);
+        assertEquals(300, g.score());
+    }
+
     private void rollStrike() {
         g.roll(10);
     }
